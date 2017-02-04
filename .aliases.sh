ea079f2e40fc977d7ee1613ddf681dc642c6ad90
Michael Ou <ougengxin@gmail.com>
2017-02-04 12:03:25 -0800

    init commit

---
+++ .aliases.sh
@@ -0,0 +1,207 @@
+
+# summa
+alias summa='~/uwhydro/summaProj/summa/bin/summa.exe'
+alias summao2='~/uwhydro/summaProj/summa/bin/summa_o2.exe'
+alias summadev='~/uwhydro/summaProj/summaNCAR/bin/summa.exe'
+alias summadevo2='~/uwhydro/summaProj/summaNCAR/bin/summa_o2.exe'
+alias summagrey='~/uwhydro/summaProj/summaGrey/bin/summa.exe'
+alias summamartyn='~/uwhydro/summaProj/summaMartyn/bin/summa.exe'
+alias buildsumma='cd ~/uwhydro/summaProj/summa/build; git pull; ./make_gfortran_local; git branch && cd -'
+alias hyaknotebook='ssh -N -f -L localhost:8888:localhost:8888 mgou@hyak.washington.edu'
+# ssh
+alias hydra='ssh -o ProxyCommand="ssh -W %h:%p gou@arctic.hydro.washington.edu" gou@hydra'
+alias hydroc1='ssh -o ProxyCommand="ssh -W %h:%p gate.ucar.edu" hydro-c1.rap.ucar.edu'
+
+# useful bash stuffs
+alias ttop="top -R -F -s 10 -o rsize"
+
+
+if [[ $OSTYPE == darwin* ]]; then
+  alias ls='ls -Gh'
+  alias cp='cp -iv'                           # Preferred 'cp' implementation
+  alias mv='mv -iv'                           # Preferred 'mv' implementation
+elif [[ $OSTYPE == cygwin* ]]; then
+  alias ls='ls -h --color=auto'
+elif [[ $OSTYPE == linux* ]]; then
+  alias ls='ls -h --color=auto'
+fi
+
+alias mkdir='mkdir -pv'                     # Preferred 'mkdir' implementation
+
+# When downloading files from the internet with wget, 
+# in almost all circumstances, you'll want to pass 
+# the -c flag in order to continue the download in 
+# case of problems. We can set that with this:
+alias wget="wget -c"
+
+
+alias dir='dir --color=auto'
+alias vdir='vdir --color=auto'
+alias grep='grep --color=auto'
+alias fgrep='fgrep --color=auto'
+alias egrep='egrep --color=auto'
+alias ll='ls -FlAhp'                       # Preferred 'ls' implementation
+alias l='ls -CF'
+alias less='less -FSRXc'                    # Preferred 'less' implementation
+alias whence='type -a'                        # where, of a sort
+
+cd() { builtin cd "$@"; ll; }               # Always list directory contents upon 'cd'
+alias cd..='cd ../'                         # Go back 1 directory level (for fast typers)
+alias ..='cd ../'                           # Go back 1 directory level
+alias ...='cd ../../'                       # Go back 2 directory levels
+alias .3='cd ../../../'                     # Go back 3 directory levels
+alias .4='cd ../../../../'                  # Go back 4 directory levels
+alias .5='cd ../../../../../'               # Go back 5 directory levels
+alias .6='cd ../../../../../../'            # Go back 6 directory levels
+alias ~="cd ~"                              # ~:            Go Home
+alias c='clear'                             # c:            Clear terminal display
+alias which='type -all'                     # which:        Find executables
+alias path='echo -e ${PATH//:/\\n}'         # path:         Echo all executable Paths
+alias show_options='shopt'                  # Show_options: display bash options settings
+alias fix_stty='stty sane'                  # fix_stty:     Restore terminal settings when screwed up
+alias cic='set completion-ignore-case On'   # cic:          Make tab-completion case-insensitive
+mcd () { mkdir -p "$1" && cd "$1"; }        # mcd:          Makes new Dir and jumps inside
+#   lr:  Full Recursive Directory Listing
+#   ------------------------------------------
+alias lr='ls -R | grep ":$" | sed -e '\''s/:$//'\'' -e '\''s/[^-][^\/]*\//--/g'\'' -e '\''s/^/   /'\'' -e '\''s/-/|/'\'' | less'
+
+#   mans:   Search manpage given in agument '1' for term given in argument '2' (case insensitive)
+#           displays paginated result with colored search terms and two lines surrounding each hit.             Example: mans mplayer codec
+#   --------------------------------------------------------------------
+    mans () {
+        man $1 | grep -iC2 --color=always $2 | less
+    }
+    
+#   showa: to remind yourself of an alias (given some part of it)
+#   ------------------------------------------------------------
+    showa () { /usr/bin/grep --color=always -i -a1 $@ ~/Library/init/bash/aliases.bash | grep -v '^\s*$' | less -FSRXc ; }
+
+#   extract:  Extract most know archives with one command
+#   ---------------------------------------------------------
+function extract {
+ if [ -z "$1" ]; then
+    # display usage if no parameters given
+    echo "Usage: extract <path/file_name>.<zip|rar|bz2|gz|tar|tbz2|tgz|Z|7z|xz|ex|tar.bz2|tar.gz|tar.xz>"
+ else
+    if [ -f $1 ] ; then
+        # NAME=${1%.*}
+        # mkdir $NAME && cd $NAME
+        case $1 in
+          *.tar.bz2)   tar xvjf ../$1    ;;
+          *.tar.gz)    tar xvzf ../$1    ;;
+          *.tar.xz)    tar xvJf ../$1    ;;
+          *.lzma)      unlzma ../$1      ;;
+          *.bz2)       bunzip2 ../$1     ;;
+          *.rar)       unrar x -ad ../$1 ;;
+          *.gz)        gunzip ../$1      ;;
+          *.tar)       tar xvf ../$1     ;;
+          *.tbz2)      tar xvjf ../$1    ;;
+          *.tgz)       tar xvzf ../$1    ;;
+          *.zip)       unzip ../$1       ;;
+          *.Z)         uncompress ../$1  ;;
+          *.7z)        7z x ../$1        ;;
+          *.xz)        unxz ../$1        ;;
+          *.exe)       cabextract ../$1  ;;
+          *)           echo "extract: '$1' - unknown archive method" ;;
+        esac
+    else
+        echo "$1 - file does not exist"
+    fi
+fi
+}
+
+# Creates an archive (*.tar.gz) from given directory.
+function compress() { tar cvzf "${1%%/}.tar.gz"  "${1%%/}/"; }
+
+# OSX stuff
+alias f='open -a Finder ./'                 # f:            Opens current directory in MacOS Finder
+alias edit='subl'                           # edit:         Opens any file in sublime editor
+trash () { command mv "$@" ~/.Trash ; }     # trash:        Moves a file to the MacOS trash
+ql () { qlmanage -p "$*" >& /dev/null; }    # ql:           Opens any file in MacOS Quicklook Preview
+alias DT='tee ~/Desktop/terminalOut.txt' # DT:           Pipe content to file on MacOS Desktop
+
+#   cdf:  'Cd's to frontmost window of MacOS Finder
+#   ------------------------------------------------------
+    cdf () {
+        currFolderPath=$( /usr/bin/osascript <<EOT
+            tell application "Finder"
+                try
+            set currFolder to (folder of the front window as alias)
+                on error
+            set currFolder to (path to desktop folder as alias)
+                end try
+                POSIX path of currFolder
+            end tell
+EOT
+        )
+        echo "cd to \"$currFolderPath\""
+        cd "$currFolderPath"
+    }
+
+    
+    
+
+function killps()   # kill by process name
+{
+    local pid pname sig="-TERM"   # default signal
+    if [ "$#" -lt 1 ] || [ "$#" -gt 2 ]; then
+        echo "Usage: killps [-SIGNAL] pattern"
+        return;
+    fi
+    if [ $# = 2 ]; then sig=$1 ; fi
+    for pid in $(my_ps| awk '!/awk/ && $0~pat { print $1 }' pat=${!#} )
+    do
+        pname=$(my_ps | awk '$1~var { print $5 }' var=$pid )
+        if ask "Kill process $pid <$pname> with signal $sig?"
+            then kill $sig $pid
+        fi
+    done
+}
+
+function mydf()         # Pretty-print of 'df' output.
+{                       # Inspired by 'dfc' utility.
+    for fs ; do
+
+        if [ ! -d $fs ]
+        then
+          echo -e $fs" :No such file or directory" ; continue
+        fi
+
+        local info=( $(command df -P $fs | awk 'END{ print $2,$3,$5 }') )
+        local free=( $(command df -Pkh $fs | awk 'END{ print $4 }') )
+        local nbstars=$(( 20 * ${info[1]} / ${info[0]} ))
+        local out="["
+        for ((j=0;j<20;j++)); do
+            if [ ${j} -lt ${nbstars} ]; then
+               out=$out"*"
+            else
+               out=$out"-"
+            fi
+        done
+        out=${info[2]}" "$out"] ("$free" free on "$fs")"
+        echo -e $out
+    done
+}
+
+
+function my_ip() # Get IP adress on ethernet.
+{
+    MY_IP=$(/sbin/ifconfig eth0 | awk '/inet/ { print $2 } ' |
+      sed -e s/addr://)
+    echo ${MY_IP:-"Not connected"}
+}
+
+function ii()   # Get current host related info.
+{
+    echo -e "\nYou are logged on ${BRed}$HOST"
+    echo -e "\n${BRed}Additionnal information:$NC " ; uname -a
+    echo -e "\n${BRed}Users logged on:$NC " ; w -hs |
+             cut -d " " -f1 | sort | uniq
+    echo -e "\n${BRed}Current date :$NC " ; date
+    echo -e "\n${BRed}Machine stats :$NC " ; uptime
+    echo -e "\n${BRed}Memory stats :$NC " ; free
+    echo -e "\n${BRed}Diskspace :$NC " ; mydf / $HOME
+    echo -e "\n${BRed}Local IP Address :$NC" ; my_ip
+    echo -e "\n${BRed}Open connections :$NC "; netstat -pan --inet;
+    echo
+}
---
+++ .bashrc.sh
@@ -0,0 +1,62 @@
+# ~/.bashrc: executed by bash(1) for non-login shells.
+
+# don't put duplicate lines or lines starting with space in the history.
+# See bash(1) for more options
+HISTCONTROL=ignoreboth
+
+# append to the history file, don't overwrite it
+shopt -s histappend
+
+# for setting history length see HISTSIZE and HISTFILESIZE in bash(1)
+export HISTFILESIZE=1000000
+export HISTSIZE=100000
+export HISTCONTROL=ignorespace
+export HISTIGNORE='ls:history:ll'
+export HISTTIMEFORMAT='%F %T '
+
+# check the window size after each command and, if necessary,
+# update the values of LINES and COLUMNS.
+shopt -s checkwinsize
+
+# If set, the pattern "**" used in a pathname expansion context will
+# match all files and zero or more directories and subdirectories.
+#shopt -s globstar
+
+ 
+# make less more friendly for non-text input files, see lesspipe(1)
+[ -x /usr/bin/lesspipe ] && eval "$(SHELL=/bin/sh lesspipe)"
+
+
+## Michael get current git branch name
+source ~/.git-prompt.sh
+usercolor='\[\e[1;33m\]'
+#usercolor='[0;31m'
+cwdcolor='\[\e[1;34m\]'
+#cwdcolor='[0;34m'
+gitcolor='\[\e[1;31m\]'
+inputcolor='\[\e[0m\]'
+#inputcolor='[0;37m'
+export PS1=${usercolor}'\u@\h'${inputcolor}': '${cwdcolor}'\w'${gitcolor}'$(__git_ps1 "(%s)")'${inputcolor}'\$ '
+#export PS1='\u \[\e'${cwdcolor}'\]\w\[\e'${gitcolor}'\]$(__git_ps1 " (%s)")\[\e'${inputcolor}'\]\$ '
+#export PS1='\[\e'${cwdcolor}'\]\w\[\e'${gitcolor}'\]$(__git_ps1 " (%s)")\[\e'${inputcolor}'\]\$ '
+#export PS1='\[\e[0;33m\]\u   \[\e[0m\]@ \[\e[0;32m\]\h \[\e[0m\]:\[\e[0;34m\]\w                                  \[\e[0m\]\$ '
+
+unset inputcolor cwdcolor gitcolor usercolor   
+
+if [ -f ~/.bash_aliases ]; then
+    . ~/.bash_aliases
+fi
+
+# enable programmable completion features (you don't need to enable
+# this, if it's already enabled in /etc/bash.bashrc and /etc/profile
+# sources /etc/bash.bashrc).
+if ! shopt -oq posix; then
+  if [ -f /usr/share/bash-completion/bash_completion ]; then
+    . /usr/share/bash-completion/bash_completion
+  elif [ -f /etc/bash_completion ]; then
+    . /etc/bash_completion
+  fi
+fi
+
+source .aliases.sh
+export LD_LIBRARY_PATH=/opt/local/lib:/usr/lib:/usr/local/lib:$LD_LIBRARY_PATH
